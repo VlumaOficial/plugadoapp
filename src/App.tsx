9e2ce229bfe848ead96ea3dae0ae46bca64ddc45
VlumaOficial <adm@vluma.com.br>
2026-06-09 12:36:34 -0300

    fix: rotas completas no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/auth/Login'
 import Cadastro from './pages/auth/Cadastro'
+import Dashboard from './pages/Dashboard'
+import CadastroConfirmacao from './pages/auth/CadastroConfirmacao'
+import EsqueciSenha from './pages/auth/EsqueciSenha'
+import RedefinirSenha from './pages/auth/RedefinirSenha'
 
 function App() {
   return (
@@ -8,6 +12,10 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/cadastro-confirmacao" element={<CadastroConfirmacao />} />
+        <Route path="/esqueci-senha" element={<EsqueciSenha />} />
+        <Route path="/redefinir-senha" element={<RedefinirSenha />} />
         <Route path="/" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
